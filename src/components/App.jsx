d9e93d06263f734367a74be4572ebed676ee9069
timDeHof <this.is.tim.dehof@gmail.com>
2022-04-02 19:30:50 -0400

    moved app.jsx to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./css/index.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
